cfa070ed094329f7704592fe945830fd8e3eb258
c.abhinav <119582971+AbhinavChintha@users.noreply.github.com>
2024-08-20 13:10:55 +0530

    Time: 0 ms (100%), Space: 41.8 MB (88.59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,17 +1,30 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        int m=matrix[0].length;
-       int[][] ans=new int[n][m];
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            ans[j][n-1-i]=matrix[i][j];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j){continue;}
+                else if(i<j){
+                    int temp=matrix[i][j];
+                    matrix[i][j]=matrix[j][i];
+                    matrix[j][i]=temp;
+                }
+                
+            }
         }
-       } 
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            matrix[i][j]=ans[i][j];
+        reverse(matrix);
+    }
+    public void reverse (int[][] matrix){
+        for(int i=0;i<matrix.length;i++){
+            int left=0;
+            int right=matrix[i].length-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
         }
-       }
     }
 }
