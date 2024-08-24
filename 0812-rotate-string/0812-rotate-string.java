59a1e4dcb443f9e01b0b7af8de51232282bf2d55
c.abhinav <119582971+AbhinavChintha@users.noreply.github.com>
2024-08-24 09:36:12 +0530

    Time: 0 ms (100%), Space: 41.6 MB (21.81%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if((s+s).contains(goal)  && s.length()==goal.length()){
+            return true;
+        }
+        return false;
+    }
+}
